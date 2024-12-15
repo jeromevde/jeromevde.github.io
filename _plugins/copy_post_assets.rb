f8f3a06cbd0f328aad8cd78be492f3759fd409c0
Van Der Elst <je.vanderelst@gmail.com>
2024-12-15 14:55:18 +0000

    fix plugin

--- _plugins/copy_post_assets.rb
+++ _plugins/copy_post_assets.rb
@@ -16,10 +16,9 @@ module Jekyll
                     next unless File.file?(asset)             # Skip directories
                     next if File.extname(asset) == ".md"      # Skip .md files
                     relative_path = asset.sub(post_directory + '/', '') # Get the relative path
-                    static_file = Jekyll::StaticFile.new(site, post_directory, site.dest, relative_path)
+                    static_file = Jekyll::StaticFile.new(site, post_directory, "", relative_path)
                     site.static_files << static_file
-                    puts "Marked for future copy: #{asset} to #{File.join(site.dest, relative_path)}"
-                end
+                    # puts "Marked for future copy: #{asset} to #{File.join(site.dest, relative_path)}"
                 end
             else
                 puts "No _posts directory found at: #{post_directory}"
