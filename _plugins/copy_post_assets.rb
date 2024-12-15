40ed721ac315958d9ac8da1ea75897f07f53c65e
Van Der Elst <je.vanderelst@gmail.com>
2024-12-15 14:30:31 +0000

    fix plugin

--- _plugins/copy_post_assets.rb
+++ _plugins/copy_post_assets.rb
@@ -4,8 +4,6 @@ module Jekyll
         # =begin
         # This plugin copies assets from the _posts directory to the _site directory.
         # It specifically processes all files but .md files.
-        # The `generate` method loops through all posts, identifies the assets in the
-        # same directory as each post, and moves them to the destination folder.
         # =end
         def generate(site)
             post_directory = File.join(site.source, '_posts') # Path to _posts directory
@@ -14,9 +12,9 @@ module Jekyll
                     next unless File.file?(asset)             # Skip directories
                     next if File.extname(asset) == ".md"      # Skip .md files
                     relative_path = asset.sub(post_directory + '/', '') # Get the relative path
-                    static_file = Jekyll::StaticFile.new(site, post_directory, "", relative_path)
+                    static_file = Jekyll::StaticFile.new(site, post_directory, site.dest, relative_path)
                     site.static_files << static_file
-                    # puts "Marked for future copy: #{asset} to #{File.join(site.dest, relative_path)}"
+                    puts "Marked for future copy: #{asset} to #{File.join(site.dest, relative_path)}"
                 end
                 end
             else
